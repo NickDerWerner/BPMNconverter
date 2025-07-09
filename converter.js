54e31a4de8b87a3f1e44c60f110d0080febe3f2d
nickwerner <ge94nez@tum.de>
2025-07-09 09:54:00 +0200

    3a changes

--- converter.js
+++ converter.js
@@ -25,7 +25,9 @@ const processJson = JSON.parse(
 
   //await fs.readFile(new URL('./processTests/claude3-7-sonnet.json', import.meta.url)) // <<< --- !!! UPDATE THIS !!!
  // await fs.readFile(new URL('./processTests/deepSeek-r1.json', import.meta.url)) // <<< --- !!! UPDATE THIS !!!
-  await fs.readFile(new URL('./processTests/deepseekC.json', import.meta.url)) // <<< --- !!! UPDATE THIS !!!
+ //await fs.readFile(new URL('./processTests/3a/originalBPMN.json', import.meta.url)) // <<< --- !!! UPDATE THIS !!!
+    await fs.readFile(new URL('./processTests/3a/dataObjectsAdded.json', import.meta.url)) // <<< --- !!! UPDATE THIS !!!
+
   //await fs.readFile(new URL('./processTests/gpt4-5.json', import.meta.url)) // <<< --- !!! UPDATE THIS !!!
   //await fs.readFile(new URL('./processTests/gpt4o.json', import.meta.url)) // <<< --- !!! UPDATE THIS !!!
   //await fs.readFile(new URL('./processTests/gpto3.json', import.meta.url)) // <<< --- !!! UPDATE THIS !!!
